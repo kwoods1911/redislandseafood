6f622c33e80db112ecb40c7259f7989c7fb8a9b1
Khari Woods <khariwoods3@gmail.com>
2024-05-18 23:41:41 -0300

    final commit

--- resources/views/pages/aboutus.blade.php
+++ resources/views/pages/aboutus.blade.php
@@ -3,8 +3,78 @@
 
 @section('content')
 <body>
-    <div>
-        <h1>About US</h1>
+    <div class="container aboutus-container">
+        <h1 class="page-header-text">RED ISLAND SEAFOOD - ATLANTIC CANADA'S MOST AFFORDABLE LOBSTER</h1>
+        
+            <div class="row">
+
+            <div class="col">
+                <img src="/assets/images/aboutuspage.jpg" alt="">
+            </div>
+
+            <div class="col">
+                <h4>Freshly Harvest/Cooked Lobster</h4>
+                <p>
+                    Indulge in the unparalleled taste of the sea with our freshly caught lobsters, 
+                    harvested from the pristine waters of Prince Edward Island, or savor the convenience of our expertly cooked lobster options, 
+                    prepared with care to bring the ocean's essence to your plate.     
+                </p>
+            </div>
+            </div>
+
+
+
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <h4>Competitive Pricing</h4>
+                    <p> At Red Island Seafood, we believe in delivering exceptional quality at an unbeatable value. 
+                        Our competitive pricing ensures that you can enjoy the finest live lobsters without compromising your budget. 
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <h4>Fast Shipping</h4>
+                    <p> Experience the convenience of our swift and secure shipping services, 
+                        available even on weekends. Every Friday evening, our dedicated truck departs from Prince Edward Island, 
+                        ensuring that your order of freshly caught or expertly cooked lobsters is delivered directly to your restaurant, 
+                        preserving the peak of their freshnes
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <h4>Expert Customer Support</h4>
+                    <p> Our dedicated customer support team is here to assist you at every step. 
+                        Whether you have questions about our products, need help placing an order, 
+                        or require guidance on preparation, our experts are ready to provide top-notch assistance with a friendly touch. 
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <h4>Easy Payment Options</h4>
+                    <p>
+                        Red Island Seafood offers convenient payment choices, 
+                        including cash (paid upon delivery), e-transfer, or credit card. To secure your order,
+                         a nominal 10% deposit is required, 
+                         ensuring a seamless and worry-free experience from selection to delivery.
+                    </p>
+                </div>
+            </div>
+
+
     </div>
 </body>
 
