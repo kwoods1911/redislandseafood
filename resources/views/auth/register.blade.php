031dbeee6d00869a0f28a3cbc74a71dcb072e9fa
Khari Woods <khariwoods3@gmail.com>
2024-05-19 01:12:23 -0300

    Decided to ask contact page styling here

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layout.default')
 
 @section('content')
 <div class="container">
@@ -63,7 +63,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary send-btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
