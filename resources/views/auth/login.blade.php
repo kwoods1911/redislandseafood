b5411c19dbe7a31750b00ed6f75dafe04755047f
Khari Woods <khariwoods3@gmail.com>
2024-05-19 23:33:18 -0300

    Added a class that controls the height of the pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.default')
 
 @section('content')
-<div class="container">
+<div class="container page-height-controller">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
