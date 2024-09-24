2227505ed0eeb4dc7da48f454795b5a43ddfeea9
Khari Woods <khariwoods3@gmail.com>
2024-09-24 20:12:02 -0300

    Final commit before checkout

--- resources/views/email_template.blade.php
+++ resources/views/email_template.blade.php
@@ -1,10 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ 'email.css' }}" type="text/css">
+    <title>Document</title>
+</head>
 <body>
     <h1>Email from {{$name}}</h1>
-    @component('mail::message')
     <span>Name: {{$name}}</span>
     <br>
     <span>{{ $email }}</span>
     <br>
     <p>{{$message}}</p>
-    @endcomponent
 </body>
+</html>
+
